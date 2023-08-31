0deca10b7031dc1eab69b6f08614ca1211fede23
Hoa Pham <phamtthanhhoa@gmail.com>
2023-09-01 03:12:53 +0700

    update test script

--- cypress/e2e/getOTPFromEmail.cy.js
+++ cypress/e2e/getOTPFromEmail.cy.js
@@ -1,33 +1,33 @@
+import { generateEmail, generatePassword } from '../support/utils'
+
 describe('Email OTP', { baseUrl: 'https://playground.mailslurp.com' }, () => {
-    let username = 'hoa.pham' + Math.floor(Math.random() * 10000).toString()
-    let password = Math.floor(Math.random() * 99 ** 9).toString()
+    let email = generateEmail()
+    let password = generatePassword()
 
     it('Register account in Mailslurp', () => {
         cy.visit('/')
 
         cy.get('[data-test="sign-in-create-account-link"]').click()
 
-        cy.get('[name=email]').type(`${username}` + '@mailsac.com')
+        cy.get('[name=email]').type(`${email}`)
         cy.get('[name=password]').type(`${password}`)
-        cy.log('username', `${username}@mailsac.com`)
+        cy.log('username', `${email}`)
         cy.log('password', `${password}`)
 
         cy.get('[data-test="sign-up-create-account-button"]').click()
 
         cy.contains('Confirm Sign Up')
 
-        cy.getOTP(`${username}@mailsac.com`, Cypress.env('API_KEY')).then(
-            () => {
-                cy.get('[name=code]').type(Cypress.env('OTP'))
+        cy.getOTP(`${email}`, Cypress.env('API_KEY')).then(() => {
+            cy.get('[name=code]').type(Cypress.env('OTP'))
 
-                cy.get('[data-test="confirm-sign-up-confirm-button"]').click()
-            }
-        )
+            cy.get('[data-test="confirm-sign-up-confirm-button"]').click()
+        })
 
         cy.contains('Sign in to your account')
 
         // need to check whether we can sign in successfully
-        cy.get('[data-test="username-input"]').type(`${username}@mailsac.com`)
+        cy.get('[data-test="username-input"]').type(`${email}`)
         cy.get('[data-test="sign-in-password-input"]').type(`${password}`)
         cy.get('[data-test="sign-in-sign-in-button"]').click()
         cy.contains('Welcome')
